3c45296925fd9065c257fb0c243670b5d48c48d9
sharun <sharunpublic@gmai.com>
2022-06-08 00:24:57 +0530

    Add first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+function toggleUserMenu() {
+    var userMenuButton = document.getElementById("menu-list");
+    userMenuButton.classList.toggle("hidden");
+}
+
+function validatePassword() {
+    var form = document.getElementById("login-form");
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+    });
+    var password = document.getElementById("password");
+    var lowercase = /[a-z]/;
+    var uppercase = /[A-Z]/;
+    var specialcase = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+    var containLowercase = lowercase.test(password.value);
+    var containUppercase = uppercase.test(password.value);
+    var containSpecialcase = specialcase.test(password.value);
+    if (!containLowercase) {
+        document.getElementById("password-error").innerHTML = "Password must contain at least one lowercase letter";
+    }
+    if (!containUppercase) {
+        document.getElementById("password-error").innerHTML = "Password must contain at least one uppercase letter";
+    }
+    if (!containSpecialcase) {
+        document.getElementById("password-error").innerHTML = "Password must contain at least one special character";
+    }
+    if (!containLowercase && !containUppercase) {
+        document.getElementById("password-error").innerHTML = "Password must contain at least one lowercase letter and one uppercase letter";
+    }
+    if (!containLowercase && !containSpecialcase) {
+        document.getElementById("password-error").innerHTML = "Password must contain at least one lowercase letter and one special character";
+    }
+    if (!containUppercase && !containSpecialcase) {
+        document.getElementById("password-error").innerHTML = "Password must contain at least one uppercase letter and one special character";
+    }
+    if (!containLowercase && !containUppercase && !containSpecialcase) {
+        document.getElementById("password-error").innerHTML = "Password must contain at least one lowercase letter, one uppercase letter and one special character";
+    }
+    if (containLowercase && containUppercase && containSpecialcase) {
+        document.getElementById("password-error").innerHTML = "";
+        var username = getUsername();
+        location.replace("/success.html?data=" + username);
+    }
+}
+
+function getUsername() {
+    var username = document.getElementById("username");
+    return username.value;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'entri-color': '#59a1c9',
+        'entri-color-dark': '#174761',
+      },
+    },
+  },
+  plugins: [],
+}
